62dea9b016f94f587dfd8b06f8f61336697d457e
iCore-034 <63407898+iCore-034@users.noreply.github.com>
2020-07-11 17:27:52 +0300

    Add files via upload

---
+++ Chapter 3/Exercise 6, chapter 3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	for (int i = 0; i < 15; i++) {  //thid cycle for checking result
+		int first = 0;				//
+		int second = 0;				// initialization for mistakes prevention
+		int third = 0;				//
+		cout << "Enter three integer: ";
+
+		cin >> first;
+		cin >> second;
+		cin >> third;
+		cout << endl;				// to look goot in cmd
+		if ((third - first) > 0 && (third - second) > 0) { // to programm knows that third is the last
+			if ((second - first) > 0) {
+				cout << first << ", " << second << ", " << third << "\n";
+			}
+			else if ((second - first) < 0)
+			{
+				cout << second << ", " << first << ", " << third << "\n";
+			}
+			else {
+				cout << second << ", " << first << ", " << third << "\n";
+			}
+
+		}
+		else if ((second - first) > 0 && (second - third) > 0) { // to programm knows that second is the last
+			if ((third - first) > 0) {
+				cout << first << ", " << third << ", " << second << "\n";
+			}
+			else if ((third - first) < 0) {
+				cout << third << ", " << first << ", " << second << "\n";
+			}
+			else
+				cout << third << ", " << first << ", " << second << "\n";
+		}
+		else {	// not the third and not the second is in the last, so first is it
+			if ((third - second) > 0) {
+				cout << second << ", " << third << ", " << first << "\n";
+			}
+			else if ((second - third) > 0) {
+				cout << third << ", " << second << ", " << first << "\n";
+			}
+			else {
+				cout << third << ", " << second << ", " << first << "\n";
+			}
+
+		}
+	}
+
+	return 0;
+} 
